46c99ca2590b70e64cc4cb5d8bb8dd47f492b227
Dominic Farquharson <farquharsondominic@gmail.com>
2024-03-11 17:09:43 -0400

    add pat to schema

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    PAT: z.string()
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    PAT: process.env.PAT,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
